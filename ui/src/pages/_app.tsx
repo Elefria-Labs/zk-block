8fae3ec05ef285fd5f973b866d2f7c22d4895594
heypran <pranjal.baweja@gmail.com>
2022-03-20 00:11:15 +0400

    fix: config

--- ui/src/pages/_app.tsx
+++ ui/src/pages/_app.tsx
@@ -5,16 +5,12 @@ import Providers from '../utils/Providers';
 
 import themes from '../themes';
 import '../styles/global.css';
-import { analytics } from '@firebase/firebase-config';
+import '@firebase/firebase-config';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   if (process.env.NEXT_PUBLIC_ENV === 'prod') {
     console.log = function () {};
   }
-
-  React.useEffect(() => {
-    analytics();
-  }, []);
 
   return (
     <Providers>
