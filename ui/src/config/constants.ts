860b0a36c25d952058fc53d877217ef29a94d347
Hugo Montenegro <h@hugomontenegro.com>
2022-09-17 15:09:49 +0200

    Fix typo

--- ui/src/config/constants.ts
+++ ui/src/config/constants.ts
@@ -6,12 +6,12 @@ export const contractAddresses: Record<string, Record<string, string>> = {
     // Harmony
     '1666600000': '0xC0fE0d0389686FDA6dB07a7cC0f20841f4872775', // replace with mainnet addr
     '1666700000': '0x190C816f1D91E5D0f231bF9cF750066783bD8C43', // testnet
-    // Ploygon
+    // Polygon
     '137': '0xB9B1E14aC876a6997734119E734A02f1720fbA78', // replace with mainnet addr
     '80001': '0x841a8095c99762Ac3cdBFda59a31af5ae8C2101D', // testnet
   },
   voting: {
-    // Ploygon
+    // Polygon
     '80001': '0x7F4fB1448D3d8a72e312Bab54368Ad1D4FF10a52',
     // Harmony Devnet
     '1666900000': '0xC0fD6B7D04858b6C1B90Bac369b18c4B5424A0d0',
